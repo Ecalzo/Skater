f739db0f446df0d85c4027652a4f208a954699bf
ecalzolaio <ecalzolaio@gmail.com>
2020-08-12 18:18:59 -0400

    test: assert function, test_refineResults function

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
+// FIXME: this requires some project restructuring
+// functions not dependent on the DOM should be moved out of popup.js
 
-/**
- * 
- */
+function assert(condition, message) {
+	if (!condition) {
+		message = message || "Assertion failed";
+		if (typeof Error !== 'undefined') {
+			throw new Error(message);
+		}
+		throw message;
+	}
+}
+
 function test_search_reddit() {
 	const searchInputELement = getSearchInputElement();
 	searchInputELement.value = 'reddit';
@@ -17,3 +26,18 @@ function test() {
 	test_search_reddit();
 	console.log('Success');
 }
+
+
+const searchResults = [
+	{title: 'Jira', url: 'https://www.atlassian.com/software/jira'},
+	{title: 'Folder', url: 'undefined'} // Firefox actually does this...
+];
+const query = 'ji';
+
+function test_refineResults() {
+	const refinedResults = refineResults(searchResults, query);
+	assert(refinedResults.length, 1);
+	assert(refinedResults[0].title === 'Jira');
+}
+
+test_refineResults();
