538e88c05ae2d00e6c00cf3ceaae94f593f50516
ecalzolaio <ecalzolaio@gmail.com>
2020-08-23 09:46:47 -0400

    css: set display: inline for search result icons

--- overlay.js
+++ overlay.js
@@ -256,6 +256,7 @@ function createListItem(result, index) {
     listIMG.src = `https://www.google.com/s2/favicons?domain_url=${domain}`;
     listIMG.style.padding = '0% 5% 0% 5%';
     listIMG.style['margin-bottom'] = '-1%';
+    listIMG.style.display = 'inline';
     listIMG.setAttribute('class', 'domain-icon');
 
     listURL.setAttribute('class', `skater-link skater-result-${index}`);
