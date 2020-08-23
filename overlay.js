89cd3bf700112408135227864e7a4602a227a24c
ecalzolaio <ecalzolaio@gmail.com>
2020-08-23 09:36:14 -0400

    js: change activation key to cmd/ctrl + j

--- overlay.js
+++ overlay.js
@@ -1,5 +1,5 @@
 document.addEventListener('keydown', documentEvent => {
-    if (documentEvent.shiftKey && documentEvent.key === 'L' && !document.querySelector('#skater-overlay')) {
+    if ((documentEvent.ctrlKey || documentEvent.metaKey) && documentEvent.key === 'j' && !document.querySelector('#skater-overlay')) {
         // const browser = chrome || browser;
         createOverlay();
         setUpInputEventListener();
