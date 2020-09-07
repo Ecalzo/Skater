47ef2b1f90594499cc94ac090ad2895e8ba59e95
ecalzolaio <ecalzolaio@gmail.com>
2020-09-07 09:10:15 -0400

    js: rm leftover comments from background script

--- background.js
+++ background.js
@@ -16,10 +16,8 @@ chrome.runtime.onMessage.addListener(
         chrome.tabs.query({url: queryUrl}, e => {
         if (e.length) { // if the tab exists, go to it
           chrome.tabs.update(e[0].id, {active: true});
-          // sendResponse(e);
         } else { // else, open a new tab
           chrome.tabs.create({url: request.url, active: true});
-          // sendResponse([]);
         }
       });
     } else {
