67e1b3e17cd90399a06a2eeb5f9403ca59adc3cb
ecalzo <evancalz@me.com>
2020-10-18 14:53:04 -0400

    tests: test for stripIndexFromClass

--- tests/content_script.test.js
+++ tests/content_script.test.js
@@ -1,4 +1,7 @@
-const {isValidInputEvent} = require('../src/utils.js');
+const {
+    isValidInputEvent,
+    stripIndexFromClass
+} = require('../src/utils.js');
 
 test('Asserts that the received keypress is one of: alphabetical, numeric, backspace, enter or shift', () => {
     expect(isValidInputEvent('A')).toBe(true);
@@ -7,4 +10,10 @@ test('Asserts that the received keypress is one of: alphabetical, numeric, backs
     expect(isValidInputEvent('5')).toBe(true);
     expect(isValidInputEvent('z')).toBe(true);
     expect(isValidInputEvent('Up')).toBe(false);
+})
+
+test('Asserts that the class id is successfully stripped from the html class string', () => {
+    const testElement = document.createElement('a');
+    testElement.setAttribute('class', `skater-link skater-result-1 skater-focused`);
+    expect(stripIndexFromClass(testElement)).toBe(1)
 })
