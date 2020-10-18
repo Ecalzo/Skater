7da1e97aec5e6cfedb2286b267215d9dd4c25fd2
Evan <evancalz@me.com>
2020-10-18 12:33:41 -0400

    js: update isValidInputEvent to match master

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,11 @@
 function isValidInputEvent(key) {
-    const isAlphabetical = (key >= "a" && key <= "z") || (key >= "A" && key <= "Z");
+    const isAlphabetical = (key >= "a" && key <= "z") || (key >= "A" && key <= "Z") && key.length === 1;
     const isNumeric = (key >= "0" && key <= "9");
     const isBackspace = (key === "Backspace");
     const isEnter = (key === "Enter");
     const isShift = (key === "Shift");
-    return isAlphabetical || isNumeric || isBackspace || isEnter || isShift
+    const isArrowKey = (['ArrowUp', 'Up', 'ArrowDown', 'Down', 'ArrowLeft', 'Left', 'ArrowRight', 'Right'].includes(key))
+    return (isAlphabetical || isNumeric || isBackspace || isEnter || isShift) && !isArrowKey
 }
 
 module.exports = {
